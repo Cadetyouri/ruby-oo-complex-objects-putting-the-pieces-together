dc97690127bec98b8d570b1f555a41a6ceca546f
Youri Cadet <cadet.jesusyouri@gmail.com>
2020-11-20 11:22:41 -0500

    commit tree e4af75852b5101fa74824c925257eb9cc38f2678

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,26 @@
+class School
+  attr_accessor :name, :roster
 
-class Book
-    attr_accessor :author, :page_count, :genre
-    attr_reader :title
-  
-    def initialize(title)
-      @title = title
-    end
-  
-    def turn_page
-      puts "Flipping the page...wow, you read fast!"
-    end
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
 
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+end
